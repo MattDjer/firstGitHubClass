57ab68409f1567d887e691fd4733aa51136aa863
Hans VMW <hans079d@stud.kea.dk>
2021-02-04 15:56:04 +0100

    Test

---
+++ Matthias/src/Test.java
@@ -0,0 +1,4 @@
+public class Test {
+    private int denneKlasseFindes = 11; //jeg højreklikkede på  src-mappen og valgte "file" --
+        // så kom der en popup frem med en lang liste af mulige filetyper, hvor jeg valgte java. Og så skrev jeg bare resten i hånden, denne fil.
+}

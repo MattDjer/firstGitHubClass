f93b7efeaa6cd948f643cf4e23fac24e3f26790b
MattDjer <74910355+MattDjer@users.noreply.github.com>
2021-02-07 18:05:55 +0100

    Matthias kvadrat class

--- Matthias/src/Main.java
+++ Matthias/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        HansKlasse HK = new HansKlasse();
-        System.out.println(HK.doesArraylistContainString2("Hans"));
+        // HansKlasse HK = new HansKlasse();
+        // System.out.println(HK.doesArraylistContainString2("Hans"));
+
+        Square square = new Square();
+
+        square.printSquare();
+
     }
 }

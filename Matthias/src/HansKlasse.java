368d05329bc5c7bc5f34f082728d12836c8e3681
Hans VMW <hans079d@stud.kea.dk>
2021-02-07 17:15:08 +0100

    First Hans 0702 2021

---
+++ Matthias/src/HansKlasse.java
@@ -0,0 +1,9 @@
+public class HansKlasse {
+    private String problemBeskrivelse = "";
+
+    public HansKlasse(){}
+
+    public void printProblemDescription(){
+        System.out.println(problemBeskrivelse);
+    }
+}

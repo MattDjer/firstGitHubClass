71af9e2d850af5fb2e3f934816bc8ef4c5d222a2
Hans VMW <hans079d@stud.kea.dk>
2021-02-07 17:45:28 +0100

    Second Hans 0702 2021 Jeg har tilfæjet min egen lektie OG en main-klasse som kan bruges til at køre lortet

---
+++ Matthias/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        HansKlasse HK = new HansKlasse();
+        System.out.println(HK.doesArraylistContainString2("Hans"));
+    }
+}
